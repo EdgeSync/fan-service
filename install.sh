1ab0b7418716fdd2c359905c127aae5ce0c30233
Chris S <truedoom@gmail.com>
2022-09-23 01:03:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo cp ./fan.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/fan.service
 
 echo "Should be installed now"
-echo "try `sudo systemctl start|stop|status fan` to start, stop and check status"
+echo "try 'sudo systemctl start|stop|status fan' to start, stop and check status"
  
